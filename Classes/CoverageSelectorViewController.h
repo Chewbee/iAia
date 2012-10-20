6dd01cdb2ad334c08527056fc72c9a58af52afa5
Georges-Henry PORTEFAIT <gh@portefait-golub.com>
2012-10-20 18:25:04 +0200

    initial commit

---
+++ Classes/CoverageSelectorViewController.h
@@ -0,0 +1,19 @@
+//
+//  CoverageSelectorViewController.h
+//  iAia
+//
+//  Created by Georges-Henry Portefait on 17/10/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "fastQuoteModel.h"
+
+
+@interface CoverageSelectorViewController : UITableViewController
+
+
+@property (nonatomic,strong)FastQuoteModel* fastQuoteModel ;
+
+
+@end
